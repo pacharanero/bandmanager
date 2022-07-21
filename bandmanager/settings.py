495611d6366bed029dc7bb1024464fa95b0e2c86
jamesbaw <jamiebaw@gmail.com>
2022-07-21 12:04:38 +0100

    adds password reset page

--- bandmanager/settings.py
+++ bandmanager/settings.py
@@ -128,5 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
