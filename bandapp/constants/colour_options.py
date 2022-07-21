c3c4e81534a980c32f2d4afd029e06303da04137
Marcus Baw <marcusbaw@gmail.com>
2022-07-21 09:12:52 +0100

    adds colour options for tags

---
+++ bandapp/constants/colour_options.py
@@ -0,0 +1,15 @@
+OPTIONS = [
+("red", "red"),
+("orange", "orange"),
+("yellow", "yellow"),
+("olive", "olive"),
+("green", "green"),
+("teal", "teal"),
+("blue", "blue"),
+("violet", "violet"),
+("purple", "purple"),
+("pink", "pink"),
+("brown", "brown"),
+("grey", "grey"),
+("black", "black"),
+]
